e11ccc89b54887c1a1be6b3c701e0ed74cb807cd
Korbinian Offensperger <korbinian_offensperger@yahoo.de>
2022-05-18 22:41:32 +0200

    first version

---
+++ MyOwn_PrimeFactorization/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOwn_PrimeFactorization
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] output = GetPrimes(15);
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------------");
+
+            output = GetPrimes(8);
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------------");
+
+            output = GetPrimes(40);
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------------");
+
+            output = GetPrimes(99);
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------------");
+
+            output = GetPrimes(256);
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------------");
+
+            output = GetPrimes(520);
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+
+        public static int[] GetPrimes(int input)
+        {
+            List<int> allPrimes = new List<int>();
+            for (int prime = 2; input != 1; prime++)
+            {
+                while (input % prime == 0 && input != 1)
+                {
+                    input /= prime;
+                    allPrimes.Add(prime);
+                }
+            }
+            return allPrimes.ToArray();
+        }
+    }
+}
